1600510576df74c347fa4228fcae3ae283cc829b
Entwoane <antoine.musci@gmail.com>
2025-05-26 17:24:30 +0200

    4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,4 +19,29 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${Array.from(firstName)[0]}. ${lastName}`;
 };
 
-console.log(printTeacher('John', 'Doe'));
+console.log(printTeacher('Gerard', 'Majax'));
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('Gerard', 'Majax');
+console.log(student.displayName());
+console.log(student.workOnHomework());
